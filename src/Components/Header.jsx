0752d0555f7f010818c50db870ca26d990ea97e3
Cyril Jodar <jodarcyril.pro@gmail.com>
2023-09-26 12:24:12 +0200

    Created React App + provided new React Rooter method

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header>
+        <nav>
+            <menu>
+                <li><a href="/">Home</a></li>
+                <li><a href="/connexion">Se connecter</a></li>
+                <li><a href="/inscription">S'inscrire</a></li>
+            </menu>
+        </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/Root.js
@@ -0,0 +1,29 @@
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import SignUp from "./Pages/SignUp";
+import SignIn from "./Pages/SignIn";
+import './Root.css';
+// Router singleton created
+const router = createBrowserRouter([
+  { path: "*", Component: Root },
+]);
+
+// RouterProvider added
+export default function App() {
+  return <RouterProvider router={router} />;
+}
+
+function Root() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/inscription" element={<SignUp />} />
+      <Route path="/connexion" element={<SignIn />} />   
+    </Routes>
+  );
+}
