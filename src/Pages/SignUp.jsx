d9bf5eb0ac120d922eb15ee3d1c75c87fb26a318
Jimmy Carlier <jimmy.carlier@lapiscine.pro>
2023-09-27 12:14:05 +0200

    Add sign up functionnalities

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,9 +1,63 @@
 const SignUp = () => {
-    return (
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const userInfo = {
+      email: e.target.email.value,
+      firstname: e.target.firstname.value,
+      lastname: e.target.lastname.value,
+      password: e.target.password.value,
+      verifPassword: e.target.verif_password.value,
+    };
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(userInfo),
+    };
+
+    const responseSignUp = await fetch(
+      `http://localhost:3000/user/signup`,
+      requestOptions
+    );
+
+    console.log(responseSignUp);
+  };
+  return (
+    <section>
+      <h1>SignUp</h1>
+      <form onSubmit={handleSubmit}>
         <div>
-            <h1>SignUp</h1>
+          <label htmlFor="">Email :</label>
+          <input type="email" id="email" name="email" required />
         </div>
-    )
-}
+        <div>
+          <label htmlFor="">Prénom : </label>
+          <input type="text" id="firstname" name="firstname" required />
+        </div>
+        <div>
+          <label htmlFor="">Nom : </label>
+          <input type="text" id="lastname" name="lastname" required />
+        </div>
+        <div>
+          <label htmlFor="">Mot de passe : </label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <div>
+          <label htmlFor="">Validation du mot de passe : </label>
+          <input
+            type="password"
+            id="verif_password"
+            name="verif_password"
+            required
+          />
+        </div>
+        <div>
+          <input type="submit" value="Je m'inscris" />
+        </div>
+      </form>
+    </section>
+  );
+};
 
 export default SignUp;
