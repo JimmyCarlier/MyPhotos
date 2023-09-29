1fcb35087c494e3ee7042b229a79633a254c37e4
Cyril Jodar <jodarcyril.pro@gmail.com>
2023-09-29 18:00:26 +0200

    CGU ARE DONE MOTHAFOCKA

--- src/Pages/PageMentionLegal.jsx
+++ src/Pages/PageMentionLegal.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import Header from "../Components/Header";
+import "../Assets/css/Pages/mentionLegal.css"
+import Footer from "../Components/Footer";
 
 const PageMentionLegal = () => {
   const [message, setMessage] = useState();
   const legalMention = async () => {
     const response = await fetch("http://localhost:3000/cgu/show");
     const responseJson = await response.json();
-    setMessage(responseJson.data);
+    setMessage({ __html : `${responseJson.data}`});
   };
 
   useEffect(() => {
@@ -16,12 +18,12 @@ const PageMentionLegal = () => {
   return (
     <>
       <Header />
-      <section>
-        <h2>CGU</h2>
-        <p>{message ? message : "Aucun texte de trouvé"}</p>
-        <h3>Mention Légal</h3>
-        <p>{message ? message : "Aucun texte de trouvé"}</p>
+      <section className="containerCGU">
+        <article dangerouslySetInnerHTML={message} className="articleCGU">
+
+        </article>
       </section>
+      <Footer />
     </>
   );
 };
