393e0ac92c716c9f8688148c5e6db4cb88a6a052
Jimmy Carlier <jimmy.carlier@lapiscine.pro>
2023-09-29 17:25:13 +0200

    add update psw for user + fix filter method

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,27 +1,68 @@
 import { Link } from "react-router-dom";
 import "../Assets/css/NavBar.css";
+import { SecurityCheckSession } from "../Components/SecurityCheckSession";
+import { useEffect, useState } from "react";
+import { handleDisconnect } from "../Components/HandleDisconnect";
 const Header = () => {
+  const [user, setUser] = useState();
+  function findUser() {
+    const user = SecurityCheckSession();
+    setUser(user);
+  }
+
+  useEffect(() => {
+    findUser();
+  }, []);
   return (
     <header>
-      <nav className="container">
-        <menu className="container-nav">
-          <div>
-            <li className="logo">
-              <Link to="/">
-                {" "}
-                <img src="/LogoMyphoto.svg"></img>
-              </Link>
-            </li>
-          </div>
-          <div className="boutton-nav">
-            <li>Concours</li>
-            <li>Voter</li>
-            <li>
-              <Link className="connexionIcon" to="/connexion"><img src="/connexion.png" alt="connexion icon" /></Link>
-            </li>
-          </div>
-        </menu>
-      </nav>
+      {!user ? (
+        <nav className="container">
+          <menu className="container-nav">
+            <div>
+              <li className="logo">
+                <Link to="/">
+                  {" "}
+                  <img src="/LogoMyphoto.svg"></img>
+                </Link>
+              </li>
+            </div>
+            <div className="boutton-nav">
+              <li>Concours</li>
+              <li>Voter</li>
+              <li>
+                <Link className="connexionIcon" to="/connexion">
+                  <img src="/connexion.png" alt="connexion icon" />
+                </Link>
+              </li>
+            </div>
+          </menu>
+        </nav>
+      ) : (
+        <nav className="container">
+          <menu className="container-nav">
+            <div>
+              <li className="logo">
+                <Link to="/">
+                  {" "}
+                  <img src="/LogoMyphoto.svg"></img>
+                </Link>
+              </li>
+            </div>
+            <div className="boutton-nav">
+              <li>Page de {user.user}</li>
+
+              <li>
+                <div className="connexionIcon">
+                  <img src="/connexion.png" alt="connexion icon" />
+                </div>
+              </li>
+              <li>
+                <button onClick={handleDisconnect}>Disconnect</button>
+              </li>
+            </div>
+          </menu>
+        </nav>
+      )}
     </header>
   );
 };
--- src/Pages/Members/EspaceMembre.jsx
+++ src/Pages/Members/EspaceMembre.jsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import { UseSession } from "../../Components/UseSession";
 import { Link } from "react-router-dom";
 import "../../Assets/css/Pages/Membres/SpaceMember.css";
+import Swal from "sweetalert2";
+import Cookies from "js-cookie";
 
 const EspaceMembre = () => {
   const [picture, setPicture] = useState([]);
@@ -32,6 +34,93 @@ const EspaceMembre = () => {
     }
   };
 
+  const handleUpdatePsw = async () => {
+    Swal.fire({
+      title: "Changer de mot de passe",
+      html: `<input type="password" id="actualPassword" class="swal2-input" placeholder="Mot de passe actuel">
+              <input type="password" id="newPassword" class="swal2-input" placeholder="Nouveau mot de passe">
+              <input type="password" id="confirmPassword" class="swal2-input" placeholder="Confirmer le mot de passe">`,
+      confirmButtonText: "Mettre à jour",
+      focusConfirm: false,
+      preConfirm: () => {
+        const actualPassword =
+          Swal.getPopup().querySelector("#actualPassword").value;
+        const newPassword = Swal.getPopup().querySelector("#newPassword").value;
+        const confirmPassword =
+          Swal.getPopup().querySelector("#confirmPassword").value;
+        if (!actualPassword || !newPassword || !confirmPassword) {
+          Swal.showValidationMessage(`Merci d'indiquer tout les champs`);
+        }
+        return {
+          actualPassword: actualPassword,
+          newPassword: newPassword,
+          confirmPassword: confirmPassword,
+        };
+      },
+    })
+      .then((result) => {
+        if (result.value.confirmPassword !== result.value.newPassword) {
+          Swal.fire(
+            `
+          Les champs ne sont pas identique
+        `.trim()
+          );
+        } else {
+          async function handleUpdate() {
+            const token = UseSession();
+            const fetchOption = {
+              method: "PUT",
+              headers: {
+                authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                actualPassword: result.value.actualPassword,
+                newPassword: result.value.newPassword,
+                confirmPassword: result.value.confirmPassword,
+              }),
+            };
+
+            const callFetch = await fetch(
+              `http://localhost:3000/user/update`,
+              fetchOption
+            );
+            console.log(callFetch);
+            if (callFetch.status === 200) {
+              Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Le mot de passe à était modifié",
+                showConfirmButton: true,
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  window.location = "/connexion";
+                  localStorage.removeItem("session");
+                  Cookies.remove("session");
+                }
+              });
+            } else {
+              Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Une erreur à était rencontré",
+                showConfirmButton: true,
+              });
+            }
+          }
+          handleUpdate();
+        }
+      })
+      .catch(() => {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Une erreur à était rencontré",
+          showConfirmButton: true,
+        });
+      });
+  };
+
   useEffect(() => {
     showAllPhotos();
   }, []);
@@ -40,8 +129,15 @@ const EspaceMembre = () => {
       <Header />
       <main>
         <section>
-          <div>
-            <Link to="/member/upload">Ajouter une photo</Link>
+          <div className="add-picture">
+            <div>
+              <Link to="/member/upload">Ajouter une photo</Link>
+            </div>
+            <div>
+              <button onClick={handleUpdatePsw}>
+                Modifier mon mot de passe
+              </button>
+            </div>
           </div>
           <div>
             <h2>Mes photos</h2>
@@ -61,7 +157,7 @@ const EspaceMembre = () => {
           </div>
           <div className="own-picture">
             {picture.length != 0 && filter.length != 0 ? (
-              picture.map((e) => {
+              filter.map((e) => {
                 return <img src={"http://localhost:3000/" + e.file} alt="" />;
               })
             ) : (
